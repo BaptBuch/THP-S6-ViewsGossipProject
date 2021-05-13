e6725040747e00b28f11600d046f3cb3037e9ae5
Bapt <baptiste.buch@gmail.com>
2021-05-13 19:20:47 +0200

    comments

--- db/migrate/20210513152617_add_gossip_to_comments.rb
+++ db/migrate/20210513152617_add_gossip_to_comments.rb
@@ -1,5 +1,5 @@
 class AddGossipToComments < ActiveRecord::Migration[5.2]
   def change
-    
+    add_reference :comments, :gossip, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_095543) do
+ActiveRecord::Schema.define(version: 2021_05_13_152617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2021_05_13_095543) do
     t.bigint "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "gossip_id"
     t.index ["comment_id"], name: "index_comments_on_comment_id"
+    t.index ["gossip_id"], name: "index_comments_on_gossip_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -71,4 +73,5 @@ ActiveRecord::Schema.define(version: 2021_05_13_095543) do
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
+  add_foreign_key "comments", "gossips"
 end
