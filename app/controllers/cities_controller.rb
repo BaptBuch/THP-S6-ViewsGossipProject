1d4d169cb93458abfb472a6545e70e5c88e3cc4c
Bapt <baptiste.buch@gmail.com>
2021-05-14 08:05:12 +0200

    commit

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,13 +2,12 @@ class CitiesController < ApplicationController
   def show
     def show
       @city = City.find(params[:id])
-      #@gossips = Gossip.all.where(user_id: params[:id])
       @gossips=[]
-      Gossip.all.each {|gossip|
+      Gossip.all.each do |gossip|
         if gossip.user.city_id == params[:id].to_i
-          @gossips.push(gossip)
+          @gossips << gossip
         end
-      }
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'cities/show'
+  
   root 'static_pages#index'
 
   get '/team', to: 'static_pages#team'
@@ -26,12 +26,6 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  resources :team, only: [:show]
+
 end
-
-# get '/welcome/:id', to: 'static_pages#welcome_user'
-
-# post '/', to: 'static_pages#welcome_id', as: 'welcome'
-
-# get '/gossip/:gossip_id', to: 'static_pages#gossip_id', as: 'gossip'
-
-# get '/user/:user_id', to: 'static_pages#user_id', as: 'user'
